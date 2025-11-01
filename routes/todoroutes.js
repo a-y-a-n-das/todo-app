55decea743f2e2396053d3c449e133a8b6d80ffa
Ayan Das <126937849+a-y-a-n-das@users.noreply.github.com>
2025-11-01 10:26:18 +0530

    Fix case sensitivity in todoControllers import

--- routes/todoroutes.js
+++ routes/todoroutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getTodos, addTodo, deleteTodo, completeTodo } = require('../controllers/todoControllers');
+const { getTodos, addTodo, deleteTodo, completeTodo } = require('../controllers/todocontrollers');
 
 
 router.get('/', getTodos);
@@ -9,3 +9,4 @@ router.delete('/', deleteTodo);
 router.patch('/', completeTodo);
 
 module.exports = router;
+
